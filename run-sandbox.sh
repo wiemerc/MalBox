c8ac694b6c9d8946c11565b002e1311f01b26d48
Constantin Wiemer <constantin.wiemer@allianz.de>
2026-09-17 11:13:34 +0200

    Set up traces/ and logs/ folders before starting the sandbox

--- run-sandbox.sh
+++ run-sandbox.sh
@@ -2,9 +2,18 @@
 
 export PODMAN_COMPOSE_WARNING_LOGS=0
 
-rm -f traces/*
+printf "Setting up folders...\n"
+rm -rf traces/
+mkdir traces/
+# Making traces/ world-writable is necessary for the SSLsplit container to be able to write into this folder because that container's
+# sslsplit user with UID 1000 gets mapped to a completely different UID on the host (behavior of rootless Podman).
+chmod 0777 traces/
+rm -rf logs
+mkdir -p logs/suricata
+
 printf "Starting containers...\n"
 podman compose up -d
+
 # With `nsenter` the `tshark` command doesn't show up in the container's process list, so the malware can't see it.
 printf "Starting tshark...\n"
 sudo nsenter -t $(podman inspect -f '{{.State.Pid}}' sandbox) -n \
@@ -21,3 +30,8 @@ printf "Stopping sysdig and tshark...\n"
 kill $sysdig_pid $tshark_pid
 printf "Stopping containers...\n"
 podman compose down
+
+printf "Fixing permissions of traces/...\n"
+chmod 0755 traces/
+sudo chown $USER:$USER traces/*
+chmod 0644 traces/*
